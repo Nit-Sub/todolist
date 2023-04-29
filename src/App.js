90ee7477a3ab7c0d20ca065e6778795af5bfd45c
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-04-29 18:15:23 +0545

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import AddTask from './components/AddTask';
+import Header from './components/Header';
+import ShowTask from './components/ShowTask';
 
 function App() {
   return (
     <>
-    <div className="App"></div>
+    <div className="App">
+    <Header/>
+    <AddTask/>
+    <ShowTask/>
+    </div>
     </>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import logo from '../assets/logo.png';
+
+const Header = () => {
+  return (
+    <>
+    <header>
+        <div className='logo'>
+            <img src={logo}/>
+            <span>ToDo List</span>
+        </div>
+        <div className="themeSelector">
+          <span className="light"></span> 
+          <span className="medium"></span> 
+          <span className="dark"></span> 
+          <span className="gOne"></span> 
+          <span className="gTwo"></span> 
+          <span className="gThree"></span> 
+        </div>
+    </header>
+
+    </>
+  )
+}
+
+export default Header
