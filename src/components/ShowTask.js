60ee73c04ab210e7dcf269e20b8d53ba398c60be
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-04-30 16:44:36 +0545

    handleDelete

--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
-const ShowTask = ({taskList , setTaskList}) => {
+const ShowTask = ({ taskList, setTaskList, task, setTask }) => {
+    const handleEdit = (id) => {
+        
+    }
+    const handleDelete = (id) => {
+        const updatedTasklist = taskList.filter(todo => todo.id !== id)
+        setTaskList(updatedTasklist);
+    }
     return (
         <>
             <section className='showTask'>
@@ -9,18 +16,18 @@ const ShowTask = ({taskList , setTaskList}) => {
                         <span className='title'>To Do</span>
                         <span className='count'>{taskList.length}</span>
                     </div>
-                    <button className='clearAll' onClick={()=>setTaskList([])}>Clear All</button>
+                    <button className='clearAll' onClick={() => setTaskList([])}>Clear All</button>
                 </div>
                 <ul>
-                    {taskList.map((todo) => 
-                    <li key={todo.id}>
-                        <p>
-                            <span className='name'>{todo.name}</span>
-                            <span className='time'>{todo.time}</span>
-                        </p>
-                        <i className='bi bi-pencil-square' ></i>
-                        <i className='bi bi-trash' ></i>
-                    </li>
+                    {taskList.map((todo) =>
+                        <li key={todo.id}>
+                            <p>
+                                <span className='name'>{todo.name}</span>
+                                <span className='time'>{todo.time}</span>
+                            </p>
+                            <i className='bi bi-pencil-square' onClick={() => handleEdit(todo.id)} ></i>
+                            <i className='bi bi-trash' onClick={() => handleDelete(todo.id)}></i>
+                        </li>
                     )}
 
                 </ul>
