2ba7db1c0bba19b0fad6814442e9183ccf04c2c5
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-05-01 17:34:38 +0545

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,20 @@ import logo from '../assets/logo.png';
 const Header = () => {
   return (
     <>
-    <header>
+      <header>
         <div className='logo'>
-            <img src={logo} alt="Logo"/>
-            <span>ToDo List</span>
+          <img src={logo} alt="Logo" />
+          <span>ToDo List</span>
         </div>
         <div className="themeSelector">
-          <span className="light"></span> 
-          <span className="medium"></span> 
-          <span className="dark"></span> 
-          <span className="gOne"></span> 
-          <span className="gTwo"></span> 
-          <span className="gThree"></span> 
+          <span className="light"></span>
+          <span className="medium"></span>
+          <span className="dark"></span>
+          <span className="gOne"></span>
+          <span className="gTwo"></span>
+          <span className="gThree"></span>
         </div>
-    </header>
+      </header>
 
     </>
   )
