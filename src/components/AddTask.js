f3685e481bfbe51d2a6e633f113e455d6a61cd90
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-05-01 14:32:49 +0545

    Update AddTask.js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,28 +3,28 @@ import React from 'react'
 const AddTask = ({ taskList, setTaskList, task, setTask }) => {
     const handleChange = (e) => {
         e.preventDefault();
-        if(task.id){
+        if (task.id) {
             const date = new Date();
             const updatedTasklist = taskList.map((todo) => (
-              todo.id === task.id ? {id: task.id, name: task.name, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`} : todo
+                todo.id === task.id ? { id: task.id, name: task.name, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}` } : todo
             ));
             setTaskList(updatedTasklist);
-          } else {
+        } else {
             const date = new Date();
             const newTask = {
-              id: date.getTime(), 
-              name: e.target.task.value, 
-              time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
+                id: date.getTime(),
+                name: e.target.task.value,
+                time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
             }
             setTaskList([...taskList, newTask]);
             e.target.task.value = "";
-          }
         }
+    }
     return (
         <>
             <section className="addTask">
                 <form onSubmit={handleChange}>
-                    <input placeholder='addTask' type='text' value={task.name} name='task' maxLength={45} autoComplete='off' onChange={(e)=> setTask({...task, name: e.target.value})}/>
+                    <input placeholder='addTask' type='text' value={task.name} name='task' maxLength={45} autoComplete='off' onChange={(e) => setTask({ ...task, name: e.target.value })} />
                     <button type='submit'>Add</button>
                 </form>
             </section>
